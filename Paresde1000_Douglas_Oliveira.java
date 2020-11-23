bd4050dd4d86ccc8e85db4e92c1f257b18dc9652
DouglasElloa <73400686+DouglasElloa@users.noreply.github.com>
2020-11-23 18:27:07 -0300

    Add files via upload

---
+++ Paresde1000_Douglas_Oliveira.java
@@ -0,0 +1,18 @@
+
+package paresde1000_douglas_oliveira;
+
+
+public class Paresde1000_Douglas_Oliveira {
+
+    
+    public static void main(String[] args) {
+       int numero=1000;
+       while (numero<1500)
+       { numero+=2;
+         System.out.println(numero);
+               
+           
+       }
+    }
+    
+}
